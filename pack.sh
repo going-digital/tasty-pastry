92552d8aa160410b55656d279ef8df5dbb18419a
Peter Knight <cathedrow@me.com>
2012-12-18 10:16:40 +0000

    Implement shorter decompression stub on all compression methods

--- pack.sh
+++ pack.sh
@@ -9,19 +9,19 @@
 # Compression effort set so compile is possible on a 256M RasPi
 
 # GZIP method
-#echo "a=/tmp/I;tail -n+2 \$0|zcat>\$a;chmod +x \$a;\$a;exit" > gz$1
-#cat $1 | gzip -c >> gz$1
-#chmod +x gz$1
+#echo "a=/tmp/I;cp \$0 \$a;tail -n+2 \$0|zcat>\$a;\$a;rm \$a;exit" > $1.pack
+#cat $1 | gzip -c >> $1.pack
+#chmod +x $1.pack
 
 # BZIP2 method
-#echo "a=/tmp/I;tail -n+2 \$0|bzcat>\$a;chmod +x \$a;\$a;rm \$a;exit" > bz$1
-#cat $1 | bzip2 -z -8 >> bz$1
-#chmod +x bz$1
+#echo "a=/tmp/I;cp \$0 \$a;tail -n+2 \$0|bzcat>\$a;\$a;rm \$a;exit" > $1.pack
+#cat $1 | bzip2 -z -8 >> $1.pack
+#chmod +x $1.pack
 
 # XZ method
-#echo "a=/tmp/I;tail -n+2 \$0|xzcat>\$a;chmod +x \$a;\$a;rm \$a;exit" > xz$1
-#cat $1 | xz -z -C none -7 >> xz$1
-#chmod +x xz$1
+#echo "a=/tmp/I;cp \$0 \$a;tail -n+2 \$0|xzcat>\$a;\$a;rm \$a;exit" > $1.pack
+#cat $1 | xz -z -C none -7 >> $1.pack
+#chmod +x $1.pack
 
 # LZMA method (seems to be consistently the best for 4k's)
 # Decompression stub overhead 54 bytes
